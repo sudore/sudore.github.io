23fc5668034f082593225518dd2f9942b40508d5
Felipe <felipepaes@disroot.org>
2019-09-30 06:19:38 -0300

    Como funciona, Cromoterapia

--- src/_assets/javascripts/application.js
+++ src/_assets/javascripts/application.js
@@ -1,28 +1,36 @@
 //= require_self
 
 
-// Smooth Scroll
+// // Smooth Scroll
+// $(document).ready(function(){
+//     // Add smooth scrolling to all links
+//     $("a.nav-link").on('click', function(event) {
+  
+//       // Make sure this.hash has a value before overriding default behavior
+//       if (this.hash !== "") {
+//         // Prevent default anchor click behavior
+//         event.preventDefault();
+  
+//         // Store hash
+//         var hash = this.hash;
+  
+//         // Using jQuery's animate() method to add smooth page scroll
+//         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+//         $('html, body').animate({
+//           scrollTop: $(hash).offset().top
+//         }, 800, function(){
+  
+//           // Add hash (#) to URL when done scrolling (default click behavior)
+//           window.location.hash = hash;
+//         });
+//       } // End if
+//     });
+//   });
+
+
 $(document).ready(function(){
-    // Add smooth scrolling to all links
-    $("a").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-  
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
-  });
+  var bgleft = document.querySelector('.left-side');
+
+  bgleft.style.backgroundImage = 'rgba(0,0,139, 0.7)';
+
+});
