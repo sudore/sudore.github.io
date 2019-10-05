37065976677dbdc059483617af131f40427b31ce
Felipe <felipepaes@disroot.org>
2019-10-05 13:46:02 -0300

    modal agendamento, return to top, responsiveness adjustments

--- src/_assets/javascripts/application.js
+++ src/_assets/javascripts/application.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     //  NavBar Effect
     var $navBar = '#main-nav';
 
-    $('body').scroll(function(){
+    $('body').scroll(function () {
         if ($('body').scrollTop() > 20) {
             $($navBar).removeClass('navbar-dark').addClass('sudore-nav navbar-light');
             console.log('if is working!');
@@ -17,6 +17,27 @@ $(document).ready(function () {
     });
 
 
+
+    /**
+     * 
+     * Back to top
+     * 
+     */
+
+    // ===== Scroll to Top ==== 
+    $('body').scroll(function () {
+        if ($(this).scrollTop() >= 50) {        // If page is scrolled more than 50px
+            $('#return-to-top').fadeIn(200);    // Fade in the arrow
+        } else {
+            $('#return-to-top').fadeOut(200);   // Else fade out the arrow
+        }
+    });
+    $('#return-to-top').click(function () {      // When arrow is clicked
+        $('body,html').animate({
+            scrollTop: 0                       // Scroll to top of body
+        }, 200);
+    });
+
 })
 
 
@@ -25,21 +46,21 @@ $(document).ready(function () {
 // $(document).ready(function(){
 //     // Add smooth scrolling to all links
 //     $("a.nav-link").on('click', function(event) {
-  
+
 //       // Make sure this.hash has a value before overriding default behavior
 //       if (this.hash !== "") {
 //         // Prevent default anchor click behavior
 //         event.preventDefault();
-  
+
 //         // Store hash
 //         var hash = this.hash;
-  
+
 //         // Using jQuery's animate() method to add smooth page scroll
 //         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
 //         $('html, body').animate({
 //           scrollTop: $(hash).offset().top
 //         }, 800, function(){
-  
+
 //           // Add hash (#) to URL when done scrolling (default click behavior)
 //           window.location.hash = hash;
 //         });
